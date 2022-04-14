1de4247705479e1967e7c36d2c4dbaf1def83310
tmdwnsasa <tmdwns_as@naver.com>
2022-04-15 00:37:41 +0900

    임시 플레이어 생성 및 체력, 마력 바 제작
    체력바 관련 리소스 추가

---
+++ EZ GAME RPG/Assets/Scripts/HpMpBar.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HpMpBar : MonoBehaviour
+{
+    public Player player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.GetChild(0).GetChild(0).gameObject.GetComponent<Image>().fillAmount = player.HP / player.MaxHP;
+        transform.GetChild(1).GetChild(0).gameObject.GetComponent<Image>().fillAmount = player.MP / player.MaxMP;
+    }
+}
---
+++ EZ GAME RPG/Assets/Scripts/Player.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private float maxhp;
+    public float MaxHP
+    {
+        get
+        {
+            return maxhp;
+        }
+    }
+
+    private float maxmp;
+    public float MaxMP
+    {
+        get
+        {
+            return maxmp;
+        }
+    }
+
+    private float hp;
+    public float HP
+    {
+        get
+        {
+            return hp;
+        }
+        set
+        {
+            hp = value;
+        }
+    }
+
+    private float mp;
+    public float MP
+    {
+        get
+        {
+            return mp;
+        }
+        set
+        {
+            mp = value;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        maxhp = 100;
+        hp = 100;
+        maxmp = 100;
+        mp = 100;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        hp -= 0.01f;
+        mp -= 0.01f;
+    }
+}
