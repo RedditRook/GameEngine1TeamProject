a5f19b6e7e6b092bb95ab071a32acbe764a74117
ghk01214 <32869007+ghk01214@users.noreply.github.com>
2022-04-21 01:25:17 +0900

    NPC 클래스 생성

---
+++ EZ GAME RPG/Assets/Scripts/BaseNPC.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseNPC : MonoBehaviour
+{
+	// 변수 목록
+	protected new string name;
+	
+	// 퀘스트, 제작, 상점
+
+    // Start is called before the first frame update
+    public virtual void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public virtual void Update()
+    {
+        
+    }
+}
---
+++ EZ GAME RPG/Assets/Scripts/SmithNPC.cs
@@ -0,0 +1,14 @@
+public class SmithNPC : BaseNPC
+{
+    // Start is called before the first frame update
+    public override void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public override void Update()
+    {
+        
+    }
+}
