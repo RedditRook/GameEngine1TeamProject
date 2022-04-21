7d7bc1e80e161ec452a7f39dea7c6fae16e4ef4f
tmdwnsasa <tmdwns_as@naver.com>
2022-04-22 02:01:49 +0900

    인벤토리 추가

--- EZ GAME RPG/Assets/Scripts/Inventory.cs
+++ EZ GAME RPG/Assets/Scripts/Inventory.cs
@@ -5,5 +5,91 @@ using UnityEngine;
 
 public class Inventory : MonoBehaviour
 {
-    //List<>;
+    public static bool invectoryActivated = false;  // ÀÎº¥Åä¸® ¿­¸² ´ÝÈû
+
+    [SerializeField]
+    private GameObject inventory_image; // Inventory_Base ÀÌ¹ÌÁö
+    [SerializeField]
+    private GameObject slots_parent;    // Inventory Grid
+
+    private InventorySlot[] slots;  // ½½·Ôµé ¹è¿­
+
+    public Item for_test;
+    public Sprite for_test_image;
+    public Item 
+    void Start()
+    {
+        slots = slots_parent.GetComponentsInChildren<InventorySlot>();
+        for_test.item_type = Item.ITEMTYPE.material;
+        for_test.name = "°¡Á×";
+        for_test.item_image = for_test_image;
+
+
+   GameObject item_prefab;
+
+}
+
+    void Update()
+    {
+        TryOpenInventory();
+    }
+
+    private void TryOpenInventory()
+    {
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            invectoryActivated = !invectoryActivated;
+
+            if (invectoryActivated)
+                OpenInventory();
+            else
+                CloseInventory();
+        }
+    }
+
+    private void OpenInventory()
+    {
+        inventory_image.SetActive(true);
+    }
+
+    private void CloseInventory()
+    {
+        inventory_image.SetActive(false);
+    }
+
+    public void GetItem(Item _item, int _count = 1)
+    {
+        if (Item.ITEMTYPE.equipment != _item.item_type)
+        {
+            for (int i = 0; i < slots.Length; i++)
+            {
+                if (slots[i].item != null)
+                {
+                    if (slots[i].item.name == _item.name)
+                    {
+                        slots[i].SetSlotCount(_count);
+                        return;
+                    }
+                }
+            }
+        }
+
+        for (int i = 0; i < slots.Length; i++)
+        {
+            if (slots[i].item == null)
+            {
+                slots[i].AddItem(_item, _count);
+                return;
+            }
+        }
+    }
+
+    public void CheatGetLeather()
+    {
+        Item CheatLeather;
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            get
+        }
+    }
 }
---
+++ EZ GAME RPG/Assets/Scripts/InventorySlot.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class InventorySlot : MonoBehaviour      //인벤토리 슬롯에 
+{
+    public Item item;           //아이템 객체
+    public TextMeshPro text;    //아이템 이름 및 겟수 출력 TMPtext
+    public int item_count;      //아이템 겟수
+    public Image item_image;    //아이템 이미지
+
+    [SerializeField]
+    private Text text_Count;
+
+    private void SetColor(float _alpha)     //아이템 투명도 조절(삭제시 알파0, 생성시 알파1)
+    {
+        Color color = item_image.color;
+        color.a = _alpha;
+        item_image.color = color;
+    }
+
+    public void AddItem(Item _item, int _count = 1) //슬롯에 아이템이 추가될 때
+    {
+        item = _item;
+        item_count = _count;
+        item_image.sprite = item.item_image;
+
+        if (item.item_type != Item.ITEMTYPE.equipment)
+        {
+            text_Count.text = item_count.ToString();
+        }
+        else
+        {
+            text_Count.text = "0";
+        }
+        SetColor(1);
+    }
+
+    public void SetSlotCount(int _count)    //슬롯에 아이템의 수가 변할 때
+    {
+        item_count += _count;
+        text_Count.text = item_count.ToString();
+
+        if (item_count <= 0)
+            ClearSlot();
+    }
+
+    private void ClearSlot()                //슬롯에 아이템이 없어질 때
+    {
+        item = null;
+        item_count = 0;
+        item_image.sprite = null;
+        SetColor(0);
+
+        text_Count.text = "0";
+    }
+}
