0346bf74e7c708e23380deedb7e69a42047a9e48
gsw0305 <gsw0305@naver.com>
2022-04-21 21:04:01 +0900

    Add Animation

--- EZ GAME RPG/Assets/Scripts/Enemy.cs
+++ EZ GAME RPG/Assets/Scripts/Enemy.cs
@@ -1,29 +1,45 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
-    public float max_health;
-    public float current_health;
-    public Transform target;
-    
-    Rigidbody rigid;
-    BoxCollider box_collider;
-    Material mat;
-    NavMeshAgent nav;
+	public float max_health;
+	public float current_health;
+	public Transform target;
+	public bool is_chase;
+	
+	Rigidbody rigid;
+	BoxCollider box_collider;
+	Material mat;
+	NavMeshAgent nav;
+	Animator anime;
 
-    void Awake()
-    {
-        rigid = GetComponent<Rigidbody>();
-        box_collider = GetComponent<BoxCollider>();
-        mat = GetComponentInChildren<SkinnedMeshRenderer>().material;
-        nav = GetComponent<NavMeshAgent>();
-    }
+	void Awake()
+	{
+		rigid = GetComponent<Rigidbody>();
+		box_collider = GetComponent<BoxCollider>();
+		mat = GetComponentInChildren<SkinnedMeshRenderer>().material;
+		nav = GetComponent<NavMeshAgent>();
+		anime = GetComponent<Animator>();
 
-     void Update()
-    {
-        nav.SetDestination(target.position);   
-    }
+		Invoke("ChaseStart", 2);
+	}
+
+	void ChaseStart()
+	{
+		is_chase = true;
+		anime.SetBool("isWalk", true);
+	}
+
+	void Update()
+	{
+		if (is_chase)
+		{
+			nav.SetDestination(target.position);
+			current_health -= 0.05f;
+		}
+
+	}
 }
