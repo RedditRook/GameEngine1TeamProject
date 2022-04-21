c2f9f32a951695c9c9c53e02cb1de96815768985
dmsdn16 <32299218+dmsdn16@users.noreply.github.com>
2022-04-21 15:35:43 +0900

    이제는 공포가 아니다

--- EZ GAME RPG/Assets/Scripts/PlayerFSM.cs
+++ EZ GAME RPG/Assets/Scripts/PlayerFSM.cs
@@ -1,18 +1,83 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerFSM : MonoBehaviour
 {
+	public enum STATE
+	{
+		IDLE,
+		WALK
+	}
+
+	public STATE currentState = STATE.IDLE;
+
+	Vector3 curTargetPos;
+
+	public float rotAnglePerSecond = 360.0f; // 1초에 플레이어의 방향을 360도 회전
+
+	public float moveSpeed = 2.0f; // 초당 2미터의 속도
+
+	PlayerAni myAni;
+
+
     // Start is called before the first frame update
     void Start()
+	{
+		myAni = GetComponent<PlayerAni>();
+		// myAni.ChangeAni(PlayerAni.ANI_WALK)
+
+		ChangeState(STATE.IDLE, PlayerAni.ANI_IDLE);
+	}
+
+	void ChangeState(STATE newState, int aniNumber)
     {
-        
+		if(currentState == newState)
+        {
+			return;
+        }
+
+		myAni.ChangeAni(aniNumber);
+		currentState = newState;
     }
 
-    // Update is called once per frame
-    void Update()
+	void UpdateState()
     {
-        
+		switch(currentState)
+        {
+			case STATE.IDLE:
+				break;
+			case STATE.WALK:
+				TurnToDes();
+				MoveToDes();
+				break;
+        }
+    }
+
+	public void MoveTo(Vector3 tPos)
+    {
+		curTargetPos = tPos;
+		ChangeState(STATE.WALK, PlayerAni.ANI_WALK);
+    }
+
+	void TurnToDes()
+    {
+		Quaternion lookRotation = Quaternion.LookRotation(curTargetPos - transform.position);
+		transform.rotation = Quaternion.RotateTowards(transform.rotation, lookRotation, Time.deltaTime * rotAnglePerSecond);
+    }
+
+	void MoveToDes()
+    {
+		transform.position = Vector3.MoveTowards(transform.position, curTargetPos, moveSpeed * Time.deltaTime);
+
+		if(transform.position == curTargetPos)
+        {
+			ChangeState(STATE.IDLE, PlayerAni.ANI_IDLE);
+        }
+    }
+	// Update is called once per frame
+	void Update()
+    {
+		UpdateState();
     }
 }
