a0f8bb28f715a471f7e04e45b12fae9f8be6da6a
tmdwnsasa <tmdwns_as@naver.com>
2022-06-16 23:00:22 +0900

    무기상인 제작

---
+++ EZ GAME RPG/Assets/Scripts/NPC/WeaponMerchant.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponMerchant : NPC
+{
+	private Shop shop;
+
+	// Start is called before the first frame update
+	public override void Start()
+	{
+		// TODO: NPC 시작 포지션 지정
+		name = "러셀";
+		shop = GameObject.Find("WeaponShopList").GetComponent<Shop>();
+	}
+
+	// Update is called once per frame
+	public override void Update()
+	{
+	}
+
+	public override void ShowUI()
+	{
+		text.PrintName(name);
+		text.PrintText("러셀 무기상에 오신 것을 환영합니다.\n");
+		text.PrintText("찾으시는 물건이 있으신가요?");
+		shop.TryOpenShop();
+		text.ShowBox();
+	}
+
+	public override void HideUI()
+	{
+		text.HideBox();
+	}
+}
