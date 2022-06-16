70c702883e0ba1a6d004c5df9d276cdf6855953d
dmsdn16 <32299218+dmsdn16@users.noreply.github.com>
2022-06-17 04:58:06 +0900

    ㅁㅁㅁㅁㅁ

--- EZ GAME RPG/Assets/Scripts/Item/SkillBar.cs
+++ EZ GAME RPG/Assets/Scripts/Item/SkillBar.cs
@@ -55,11 +55,6 @@ public class SkillBar : MonoBehaviour
 
 	public void SetLevelText(int level)
 	{
-<<<<<<< Updated upstream
-		//Debug.Log(level);
-=======
-		
->>>>>>> Stashed changes
 		leveltext.text = "LV : " + level;
 	}
 }
